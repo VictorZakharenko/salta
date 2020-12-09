2ada168db93bf1e569150b4730e96cde1dfd7f19
cider_sweet <victor.zakharenko@yahoo.com>
2020-12-10 00:58:02 +0200

    forget

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,10 +4,10 @@ from wtforms.validators import DataRequired, Regexp
 
 class GroupIdsForm(FlaskForm):
 
-    group_id1 = StringField('group_id', validators=[DataRequired(), Regexp('^[0-9]+$', message='Only numbers')])
-    group_id2 = StringField('group_id', validators=[DataRequired(), Regexp('^[0-9]+$', message='Only numbers')])
-    group_id3 = StringField('group_id', validators=[DataRequired(), Regexp('^[0-9]+$', message='Only numbers')])
-    group_id4 = StringField('group_id', validators=[DataRequired(), Regexp('^[0-9]+$', message='Only numbers')])
-    group_id5 = StringField('group_id', validators=[DataRequired(), Regexp('^[0-9]+$', message='Only numbers')])
-    group_id6 = StringField('group_id', validators=[DataRequired(), Regexp('^[0-9]+$', message='Only numbers')])
+    group_id1 = StringField('Group ID', validators=[DataRequired(), Regexp('^[0-9]+$', message='Only numbers')])
+    group_id2 = StringField('Group ID', validators=[DataRequired(), Regexp('^[0-9]+$', message='Only numbers')])
+    group_id3 = StringField('Group ID', validators=[DataRequired(), Regexp('^[0-9]+$', message='Only numbers')])
+    group_id4 = StringField('Group ID', validators=[DataRequired(), Regexp('^[0-9]+$', message='Only numbers')])
+    group_id5 = StringField('Group ID', validators=[DataRequired(), Regexp('^[0-9]+$', message='Only numbers')])
+    group_id6 = StringField('Group ID', validators=[DataRequired(), Regexp('^[0-9]+$', message='Only numbers')])
     submit = SubmitField('Go')
